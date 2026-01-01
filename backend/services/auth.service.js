30f39a188d2cfaa210e3802f780a0143a9ab7fdf
Shreyas Jaiswal <shreyasjaiswal123@gmail.com>
2026-01-01 21:23:56 +0530

    Fixed CORS issue

--- backend/services/auth.service.js
+++ backend/services/auth.service.js
@@ -10,7 +10,7 @@ export const handleUserDataSend = async (res, user) => {
   res.cookie('auth', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
 
